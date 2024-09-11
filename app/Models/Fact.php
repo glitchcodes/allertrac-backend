aa7b0a85747d8f09b41137de59e65a9ccb3d9209
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-09-12 03:35:02 +0800

    refactor: append storage url to fact's cover_image

--- app/Models/Fact.php
+++ app/Models/Fact.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Storage;
 
 class Fact extends Model
 {
@@ -40,5 +42,19 @@ class Fact extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(FactCategory::class, 'category_id');
+    }
+
+    /**
+     * Get the cover image of the fact.
+     *
+     * Automatically prepends the storage URL to the cover image path.
+     *
+     * @return Attribute
+     */
+    protected function coverImage(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Storage::disk('backblaze')->url($value)
+        );
     }
 }
