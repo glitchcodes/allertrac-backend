bef713cfab144f03b78ec95a3a54d05cb71c97e7
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-11-22 11:30:54 +0800

    fix: generate otp for unverified users on login

--- app/Actions/User/LoginUser.php
+++ app/Actions/User/LoginUser.php
@@ -2,14 +2,21 @@
 
 namespace App\Actions\User;
 
+use App\Actions\OTP\GenerateOTP;
 use App\Enums\HttpCodes;
+use App\Mail\ConfirmAccount;
 use App\Traits\ApiResponseTrait;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 readonly class LoginUser
 {
     use ApiResponseTrait;
+
+    public function __construct(
+        private GenerateOTP $otpGenerator
+    ) {}
 
     public function execute(array $credentials): JsonResponse
     {
@@ -26,11 +33,19 @@ readonly class LoginUser
 
         // If the credentials are correct, check if the user's email is verified
         if (!$user->isEmailVerified()) {
+            // Generate OTP and send it to the user's inbox
+            $otp = $this->otpGenerator->execute('email-verification', $user->id);
+
+            Mail::to($credentials['email'])->queue(new ConfirmAccount($otp['code']));
+
             return $this->sendErrorResponse(
                 'Your email is not verified',
-                HttpCodes::FORBIDDEN->value,
+                HttpCodes::ACCESS_DENIED->value,
                 null,
-                HttpCodes::FORBIDDEN->getHttpStatusCode()
+                HttpCodes::ACCESS_DENIED->getHttpStatusCode(),
+                [
+                    'identifier' => base64_encode($otp['identifier'])
+                ]
             );
         }
 
